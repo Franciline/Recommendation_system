e2a9fb70cd1c77a65b6835507e08620b78c75ba9
Lyna Combo <lyna.combo@gmail.com>
2025-02-06 20:01:09 +0100

    filtrage : avec dépendance

--- data_visualization/filtrage_jeux_users.py
+++ data_visualization/filtrage_jeux_users.py
@@ -3,9 +3,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Nombre maximum de
+def drop_min_avis(df : pd.DataFrame, avis_min : int) -> pd.DataFrame : 
+    """
+    Retire garde les users ayant un min d'avis ok
+    """
+    df_copy = df.copy()
+    # Côté users
+    users = df.groupby('User id').size().reset_index(name='size')
+    users_in = users.loc[ users["size"] < avis_min, ['User id']]
+
+    df_copy = df_copy.drop(df_copy[np.isin(df_copy["User id"], users_in.to_numpy()) ].index)
+
+    # Côté jeux
+    jeux = df.groupby('Game id').size().reset_index(name='size')
+    jeux_in = jeux.loc[ jeux["size"] < avis_min,['Game id']]
+
+    df_copy = df_copy.drop(df_copy[np.isin(df_copy["Game id"],jeux_in.to_numpy()) ].index)
+    return df_copy 
+
 
 def get_nb_users_avis(df : pd.DataFrame, depedency = False) -> np.ndarray:
-    """ Retourne un array de 1 à max(nb_avis/jeux, nb_avis/users)
+    """ Retourne un array de 1 à max(nb_avis/jeux, nb_avis/user
     
     Parameters:
     --------
@@ -16,8 +34,8 @@ def get_nb_users_avis(df : pd.DataFrame, depedency = False) -> np.ndarray:
     """ 
     if(not(depedency)):
         # Création de l'axe des X
-        jeux = df.groupby("User id").size().to_frame('size')
-        users = (df.groupby("Game id")).size().to_frame('size')
+        users = df.groupby("User id").size().to_frame('size')
+        jeux = (df.groupby("Game id")).size().to_frame('size')
         maxi = max(np.max(jeux["size"]), np.max(users["size"]))
     
         
@@ -31,7 +49,12 @@ def get_nb_users_avis(df : pd.DataFrame, depedency = False) -> np.ndarray:
         y_data_users = vect_users(x_data)
 
     else:
-        return None
+        x_data = np.arange(start=25, stop=1000, step=1)
+        vect_jeux = np.vectorize(lambda x : (len(drop_min_avis(df,x).groupby("Game id"))))
+        vect_users = np.vectorize(lambda x : (len(drop_min_avis(df,x).groupby("User id"))))
+
+        y_data_jeux = vect_jeux(x_data)
+        y_data_users = vect_users(x_data)
 
     # Création du graphique
     print("Affichage")
@@ -46,4 +69,4 @@ def get_nb_users_avis(df : pd.DataFrame, depedency = False) -> np.ndarray:
 folder = "../database_cleaned"
 avis_clean = pd.read_csv(f'{folder}/avis_clean.csv', header=None, names=["Game id", "User id", "Game name UI", "Username", "Datetime", "Rating", "Comment title", "Comment body"]).drop_duplicates()
 
-get_nb_users_avis(avis_clean)
+get_nb_users_avis(avis_clean, depedency=True)
